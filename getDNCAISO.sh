46bf3587f4f639243e38ae23d73114d9c64059b5
vcwork2019 <141845623+vcwork2019@users.noreply.github.com>
2025-08-14 22:22:45 +0800

    Update getDNCAISO.sh

--- getDNCAISO.sh
+++ getDNCAISO.sh
@@ -5,10 +5,15 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 echo ""
 echo "merging DNCA1220-ISO.iso from 7-zip files..."
 echo "destination folder: /root/tmp/"
+echo ""
 sudo mkdir /root/tmp && cd /root/tmp
 
 if ! command -v 7z &> /dev/null; then
+echo "7z:command not found, will install 7z first."
 sudo dnf install -y epel-release
 sudo dnf install -y p7zip p7zip-plugins
 fi
+
+sleep 2
+echo ""
 7z x DNCA1220-ISO.7z.001
